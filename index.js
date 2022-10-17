ae073bffeb3e66c3e5f8213777bd1355fd59f847
sumeet <sumeettheracer@gmail.com>
2022-10-17 12:52:16 +0530

    change done

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const app = express();
 app.get("/",function(req,res){
     res.send("<h1>Hello express is running</h1>")
 })
-app.listen(3000,function(){
+app.listen(process.env.PORT||3000,function(){
     console.log("the server is running in the port 3000.")
 })
